aa6d77560f54e8071021196e6cc8a604ef50d35c
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2022-07-01 01:26:58 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,6 +29,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="team.html">My Team</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://instagram.com/maybe_shashank">Instagram</a><li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="http://python.theshashank.me/">Python code</a><li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
